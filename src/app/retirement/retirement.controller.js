017bd55d8b8cf2febb897e3ae9f6f35c70cbac16
rickithadi <rickithadi@gmail.com>
2017-07-11 14:46:59 +0800

    trying to add service to the retirement thing

--- src/app/retirement/retirement.controller.js
+++ src/app/retirement/retirement.controller.js
@@ -1,14 +1,19 @@
-angular.module('inspinia').controller('retireController', function($scope) {
+angular.module('inspinia').controller('retireController', function($scope,$location) {
 
-     $scope.apply = false;
+  $scope.apply = false;
+  $scope.income = 6000;
 
-  var test=5;
-$scope.percentage=0;
+  $scope.percentage = 70;
+ //$scope.retIncome=$scope.percentage/100 *$scope.income;
+  $scope.redirect = function() {
+     $scope.retIncome=$scope.percentage/100 *$scope.income;
+    $location.url('/retirement2.html');
+  }
   $scope.slider_init_options = {
     min: 0,
     max: 150,
     from: 70,
-    step: 10,
+    step: 5,
     type: 'single',
     postfix: "% ",
     prettify: true,
@@ -16,7 +21,7 @@ $scope.percentage=0;
     hasGrid: true,
     onFinish: function(data) {
       if (data.input.attr('id') == 'slider_init') {
-        console.log('slider_init:' , data.from);
+        console.log('slider_init:', data.from);
 
         $scope.percentage = data.from
         console.log($scope.percentage);
