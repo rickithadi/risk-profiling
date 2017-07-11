1acefac584a8b20532de6aa79d141b9892525820
rickithadi <rickithadi@gmail.com>
2017-07-11 12:02:46 +0800

    put slider into a container and modified value but cant get values to print onto the view

--- src/app/retirement/retirement.controller.js
+++ src/app/retirement/retirement.controller.js
@@ -1,34 +1,29 @@
-angular.module('inspinia').controller('retireController', function ($scope) {
+angular.module('inspinia').controller('retireController', function($scope) {
 
-
-
+  var test=5;
+$scope.percentage=0;
   $scope.slider_init_options = {
-  				min : 0,
-  				max : 1000000,
-  				from : 0,
-  				step: 5000,
-  				type : 'single',
-  				prefix : "$ ",
-  				maxPostfix : "+",
-  				prettify : false,
-  				grid : true,
-  				hasGrid : true,
-  				onFinish : onfinish
-  			};
+    min: 0,
+    max: 150,
+    from: 70,
+    step: 10,
+    type: 'single',
+    postfix: "% ",
+    prettify: true,
+    grid: true,
+    hasGrid: true,
+    onFinish: function(data) {
+      if (data.input.attr('id') == 'slider_init') {
+        console.log('slider_init:' , data.from);
+        test = data.from;
+        console.log("test", test);
+        $scope.percentage = data.from
+        console.log($scope.percentage);
 
+      }
 
-  function onfinish(data) {
-  	if(data.input.attr('id') == 'slider_init')
-  	{
-  		console.log('slider_init:' + data.from);
+    }
 
-  	}
-
-  // RUN YOUR CODE HERE
-  }
-
-
-
-
+  };
 
 })
