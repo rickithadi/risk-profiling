8f68017b61d78a9b6f0870a3e0811627c7f66c6f
rickithadi <rickithadi@gmail.com>
2017-07-13 11:36:10 +0800

    finally got values over from service

--- src/app/retirement/retirement.controller.js
+++ src/app/retirement/retirement.controller.js
@@ -1,44 +1,14 @@
 angular.module('inspinia').controller('retireController', function(retireService, $scope, $window) {
 
   $scope.apply = false;
-var penis=0;
-//  $scope.income;
+$scope.name=retireService.getName();
+
+  $scope.income=retireService.getIncome();
+
+  console.log("loacl income", $scope.income)
   //$scope.retIncome=0;
-  $scope.percentage =70;
+  $scope.percentage=retireService.getPercentage();
   $scope.retIncome = $scope.percentage / 100 * $scope.income;
-
-
-  // $scope.setName = function(name) {
-  //   console.log("setName")
-  //   retireService.setName(name);
-  // }
-  // $scope.getName = function() {
-  //   console.log("getName")
-  //   retireService.getName();
-  // }
-  // $scope.setIncome = function(income) {
-  //   console.log("setIncome")
-  //   retireService.setIncome(income);
-  // }
-  $scope.getIncome = function() {
-    console.log("getIncome")
-    retireService.getIncome();
-    retireService.income=$scope.income;
-    console.log("local income", $scope.income)
-  }
-  // $scope.setPercentage = function(percentage) {
-  //   retireService.setPercentage();
-  // }
-  $scope.getPercentage = function() {
-    retireService.getPercentage();
-    retireService.percentage=$scope.percentage;
-  }
-
-  $scope.redirect = function() {
-    $scope.retIncome = $scope.percentage / 100 * $scope.income;
-    console.log("income for retiremnt:", $scope.retIncome);
-    $window.location.href = '#/retirement/retirement2';
-  }
 
   $scope.slider_init_options = {
     min: 0,
@@ -63,11 +33,15 @@ var penis=0;
 
   };
 
-  var init = function () {
-      $scope.getIncome();
-      $scope.getPercentage();
-};
-// and fire it after definition
-init();
+
+//   var init = function () {
+//     console.log("loacl inc", $scope.income)
+//     //  $scope.getIncome();
+//       $scope.getPercentage();
+//       $scope.getName();
+//       console.log("hi")
+// };
+// // and fire it after definition
+// init();
 
 })
